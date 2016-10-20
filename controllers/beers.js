d470bc9460b80155370582e7d30b957d3c167534
btchae <bchae@nyu.edu>
2016-10-20 10:56:21 -0400

    added beer query in the controller

--- controllers/beers.js
+++ controllers/beers.js
@@ -7,9 +7,21 @@ router.get('/', function(req, res) {
   console.log('testing controller');
 });
 
+router.get('/:beer', function(req, res) {
+  console.log('testing beer search');
+  request('http://api.brewerydb.com/v2/search?q='+ req.params.beer + '&type=beer&key=' + 
+    process.env.BEER_KEY + '&format=json', function(error, response, body) {
+      if(!error && response.statusCode == 200) {
+        console.log('API data below...');
+        console.log(body);
+      }
+  });
+});
+
 router.get('/search/:longitude/:latitude', function(req, res) {
   console.log('testing search');
-  request('https://api.brewerydb.com/v2/search/geo/point?lat='+'35.772096'+'&lng='+'-78.638614'+'&key='+process.env.BEER_KEY+'&format=json');
+  request('https://api.brewerydb.com/v2/search/geo/point?lat='+'35.772096'+'&lng='+
+    '-78.638614'+'&key='+process.env.BEER_KEY+'&format=json');
 });
 
 module.exports = router;
